ca36f15dacb711361497bff8fd5ec0eb306633d4
Doowankim <rlaendhks11@gmail.com>
2019-11-17 14:47:27 +0900

    movie and tv detail api

--- src/api.js
+++ src/api.js
@@ -12,11 +12,23 @@ const api = axios.create({
 export const moviesApi = {
     nowPlaying: () => api.get("movie/now_playing"),
     upcoming: () => api.get("movie/upcoming"),
-    popular: () => api.get("movie/popular")
+    popular: () => api.get("movie/popular"),
+    movieDetail: id =>
+        api.get(`movie/${id}`, {
+            params: {
+                append_to_response: "videos"
+            }
+        })
 };
 
 export const tvApi = {
     popular: () => api.get("tv/popular"),
     topRated: () => api.get("tv/top_rated"),
-    airingToday: () => api.get("tv/airing_today")
+    airingToday: () => api.get("tv/airing_today"),
+    tvDetail: id =>
+        api.get(`tv/${id}`, {
+            params: {
+                append_to_response: "videos"
+            }
+        })
 };
