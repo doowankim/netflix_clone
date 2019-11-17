542d5c84b43d6fb7bb3bea2102f7f220ae2629a8
Doowankim <rlaendhks11@gmail.com>
2019-11-17 15:50:22 +0900

    add section

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
-import loader from "../../component/loader";
+import loader from "../../component/Loader";
+import Section from "../../component/Section";
 
 const Container = styled.div`
   padding: 0px 10px;
@@ -14,25 +15,25 @@ const HomePresenter = ({ popular, upcoming, nowPlaying, loading, error }) =>
     ) : (
         <Container>
             {nowPlaying && nowPlaying.length > 0 && (
-                <div>
+                <Section title="Now Playing">
                     {nowPlaying.map(movie =>
                         <span key={movie.id}>{movie.title}</span>
                     )}
-                </div>
+                </Section>
             )}
             {popular && popular.length > 0 && (
-                <div>
+                <Section title="Popular">
                     {popular.map(movie =>
                         <span key={movie.id}>{movie.title}</span>
                     )}
-                </div>
+                </Section>
             )}
             {upcoming && upcoming.length > 0 && (
-                <div>
+                <Section title="Upcoming">
                     {upcoming.map(movie =>
                         <span key={movie.id}>{movie.title}</span>
                     )}
-                </div>
+                </Section>
             )}
         </Container>
     );
